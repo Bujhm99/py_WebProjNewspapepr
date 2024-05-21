3c190b9d480f60865395991b58424c87e4015bba
bujhm <Bujhm99@gmail.com>
2024-05-21 13:06:46 +0300

    add allowed hosts

--- py_WebProjNewspapepr/settings.py
+++ py_WebProjNewspapepr/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-8ovil3xu6=eaoq
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 # DEBUG =False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "py-webprojnewspapepr.onrender.com"]
 
 
 # Application definition
